fb1c3707361ef5f870f68df36405b51a82dc1e70
yasu <yasu.n.0430@gmail.com>
2017-02-20 02:35:42 +0000

    profileすきゃった

---
+++ db/migrate/20170220023224_create_profiles.rb
@@ -0,0 +1,11 @@
+class CreateProfiles < ActiveRecord::Migration[5.0]
+  def change
+    create_table :profiles do |t|
+      t.string :image
+      t.text :hobby
+      t.timestamp :birthday
+
+      t.timestamps
+    end
+  end
+end
