e58bfbc69798f1f586c728195f95e37028a0b461
yasu <yasu.n.0430@gmail.com>
2017-02-21 02:01:55 +0000

    profile機能作成

--- db/migrate/20170221013654_create_profiles.rb
+++ db/migrate/20170221013654_create_profiles.rb
@@ -2,7 +2,7 @@ class CreateProfiles < ActiveRecord::Migration[5.0]
   def change
     create_table :profiles do |t|
       t.string :image
-      t.text :hobby
+      t.string :hobby
       t.timestamp :birthday
 
       t.timestamps
