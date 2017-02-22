bd9753b3e83b90c694912b811d8750eca484a7c0
hyenoh <hyonzioyazi@gmail.com>
2017-02-22 02:21:07 +0000

    一旦プッシュ

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
-  resources :events
+  devise_for :users, skip: [:sessions]
+  devise_scope :user do
+    get 'login' => 'devise/sessions#new'
+    post 'login' => 'devise/sessions#create'
+    get 'logout' => 'devise/sessions#destroy'
+    get ''
+  end
+  root :to => "home#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
