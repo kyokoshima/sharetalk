569f3b58787bdb3d7ad23bb4253b5b213c45be7a
lin-chiang <n.hayashi@sparks.link>
2017-02-23 10:21:55 +0800

    Timelineトップに投稿フォーム

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20170221022058) do
+
+  create_table "events", force: :cascade do |t|
+    t.string   "title"
+    t.datetime "date"
+    t.string   "place"
+    t.text     "detail"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "groups", force: :cascade do |t|
+    t.string   "name"
+    t.text     "detail"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "profiles", force: :cascade do |t|
+    t.string   "image"
+    t.string   "hobby"
+    t.datetime "birthday"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "timelines", force: :cascade do |t|
     t.text     "content"
@@ -19,26 +42,4 @@ ActiveRecord::Schema.define(version: 20170221022058) do
     t.datetime "updated_at", null: false
   end
 
-
-
-
-  create_table "profiles", force: :cascade do |t|
-    t.string   "image"
-    t.string   "hobby"
-    t.datetime "birthday"
-  end
-
-  create_table "groups", force: :cascade do |t|
-    t.string   "name"
-  end
-  create_table "events", force: :cascade do |t|
-    t.string   "title"
-    t.datetime "date"
-    t.string   "place"
-
-    t.text     "detail"
-
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 end
