213d89e5e0bfe8cf1a31e1de5a26e8aeabf772a6
lin-chiang <n.hayashi@sparks.link>
2017-02-28 09:46:20 +0800

    tempolary commit for maketimeline

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170222013715) do
+ActiveRecord::Schema.define(version: 20170227084315) do
 
   create_table "events", force: :cascade do |t|
     t.string   "title"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20170222013715) do
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
     t.string   "name"
+    t.integer  "user_id"
   end
 
   create_table "timelines", force: :cascade do |t|
