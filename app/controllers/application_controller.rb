a5486690957d0d03a73082209ac24b87ddabfe68
yasuyukiacthouse <yasu.n.0430@gmail.com>
2017-03-03 09:47:37 +0000

    login後のredirect先変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   def after_sign_in_path_for(resource)
-    new_profile_path
+    root_path
   end
+  
+
 
   def after_sign_out_path_for(resource)
-    profiles_path
+    new_user_session_path
   end
 
 end
