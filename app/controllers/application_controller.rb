44ef2c5b83729d7f85717a3c885cb99ec6316b31
yasuyukiacthouse <yasu.n.0430@gmail.com>
2017-03-03 02:08:04 +0000

    ユーザー登録後profile作成に飛ぶ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  def after_sign_in_path_for(resource)
+    new_profile_path
+  end
 
+  def after_sign_out_path_for(resource)
+    profiles_path
+  end
 
 end
