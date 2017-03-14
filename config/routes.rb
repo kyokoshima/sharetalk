4ba2a455e0706de8ae38bc3b81a80300c61908ca
植原大樹 <daiking@zhiyuandashus-MacBook-Pro.local>
2017-03-14 09:42:22 +0800

    change login_url

--- config/routes.rb
+++ config/routes.rb
@@ -4,33 +4,21 @@ Rails.application.routes.draw do
   resources :turns do
     resources :turn_details
   end
-  # devise_for :users, controllers: { registrations: :registrations }
-
+  
   resources :expenses do
   	resources :costs
   end
 
-
-
+  devise_for :users, skip: [:sessions, :registrations] #deviseのデフォルトのpathを削除
   devise_scope :user do
-    # root :to => "devise/sessions#new"
+    root :to => "devise/sessions#new"
     get 'login' => 'devise/sessions#new', as: :new_user_session
     post 'login' => 'devise/sessions#create', as: :user_session
-    delete 'logout' => 'devise/sessions#destroy', as: :destroy_user_session
-    get 'signup' => 'devise_invitable/registrations#new', as: :new_user_registration
-    post 'signup' => 'devise_invitable/registrations#create', as: :user_registration
-    get 'signup/cancel' => 'devise_invitable/registrations#cancel', as: :cancel_user_registration
-    get 'user' => 'devise_invitable/registrations#edit', as: :edit_user_registration
-    patch 'user' => 'devise_invitable/registrations#update', as: nil
-    put 'user' => 'devise_invitable/registrations#update', as: :update_user_registration
-    delete 'user' => 'devise_invitable/registrations#destroy', as: :destroy_user_registration
-    get 'password' => 'devise/passwords#new', as: :new_user_password
-    post 'password' => 'devise/passwords#create', as: :user_password
-    get 'password/edit' => 'devise/passwords#edit', as: :edit_user_password
-    patch 'password' => 'devise/passwords#update'
-    put 'password' => 'devise/passwords#update', as: :update_user_password
-end
-  resources :users
+    get 'logout' => 'devise/sessions#destroy', as: :destroy_user_session
+    get 'sign_up' => 'devise/registrations#new', as: :new_user_registration
+    post 'sign_up' => 'devise/registrations#create', as: :user_registration
+  end
+
   resources :messages
 
   root :to => "home#index"
