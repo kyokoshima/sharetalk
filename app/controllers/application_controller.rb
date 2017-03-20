dda51852de7594fd770ba752e6b4a0e1d1f945a6
kyokoshima <dev.kyoko@gmail.com>
2017-03-20 09:14:14 +0800

    add devise message for japanese

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   layout :layout_by_resource
+  before_action :authenticate_user!
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
