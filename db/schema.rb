9b36bf8ea8a81e6b56d747485585b97a14a74d73
DAIKI03 <daikiuehara3@gmail.com>
2017-03-22 00:51:01 +0000

    nnn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170301094005) do
+ActiveRecord::Schema.define(version: 20170310085256) do
 
   create_table "costs", force: :cascade do |t|
     t.string   "subject"
@@ -28,6 +28,15 @@ ActiveRecord::Schema.define(version: 20170301094005) do
     t.text     "detail"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "expense_users", force: :cascade do |t|
+    t.integer  "expense_id"
+    t.integer  "user_id"
+    t.integer  "position"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["expense_id", "user_id"], name: "index_expense_users_on_expense_id_and_user_id"
   end
 
   create_table "expenses", force: :cascade do |t|
@@ -50,6 +59,26 @@ ActiveRecord::Schema.define(version: 20170301094005) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "message_group_users", id: false, force: :cascade do |t|
+    t.integer "message_group_id"
+    t.integer "user_id"
+    t.index ["message_group_id"], name: "index_message_group_users_on_message_group_id"
+    t.index ["user_id"], name: "index_message_group_users_on_user_id"
+  end
+
+  create_table "message_groups", force: :cascade do |t|
+    t.string   "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+    t.string   "content"
+    t.integer  "message_group_id"
+  end
+
   create_table "profiles", force: :cascade do |t|
     t.string   "image"
     t.string   "hobby"
@@ -64,11 +93,40 @@ ActiveRecord::Schema.define(version: 20170301094005) do
     t.integer  "user_id"
   end
 
+  create_table "read_marks", force: :cascade do |t|
+    t.string   "readable_type", null: false
+    t.integer  "readable_id"
+    t.string   "reader_type",   null: false
+    t.integer  "reader_id"
+    t.datetime "timestamp"
+    t.index ["reader_id", "reader_type", "readable_type", "readable_id"], name: "read_marks_reader_readable_index", unique: true
+  end
+
   create_table "timelines", force: :cascade do |t|
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
+  end
+
+  create_table "turn_detail_users", force: :cascade do |t|
+    t.integer  "turn_detail_id"
+    t.integer  "user_id"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+  end
+
+  create_table "turn_details", force: :cascade do |t|
+    t.string   "when"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "turn_id"
+  end
+
+  create_table "turns", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
